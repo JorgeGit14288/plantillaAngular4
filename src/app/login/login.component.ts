6f6978897a0b8fb1e3a432cae118051aff61966d
Jorge14288 <jorge.fuentes.14288@gmail.com>
2018-01-31 13:38:12 -0600

    se agrego un mensaje de error al login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-// importamos el modelo 
+// importamos el modelo
 import { LoginModel } from './../models/login_model';
 
 
@@ -15,24 +15,26 @@ import { LoginModel } from './../models/login_model';
 export class LoginComponent implements OnInit {
 
   public model = new LoginModel(null, null);
-
+  public mensaje = '';
+  public mostrar = false;
   onSubmit() {
-    if (this.model.username == '' || this.model.password=='')
+    if (this.model.username == '' || this.model.password =='')
     {
-
-      console.log(' campos vacios '); 
+      console.log(' campos vacios ');
+      this.mensaje = 'Los campos estan vacios';
     }
-    else
-    {
-      if(this.model.username=='jorge' && this.model.password=='hola')
+    else {
+      if(this.model.username === 'jorge' && this.model.password === 'hola')
       {
         console.log(' login completo ');
-        this.router.navigateByUrl('/dashboard');
+        this.mensaje = 'Login Valido';
+         this.router.navigateByUrl('/dashboard');
+      }
+      else{
+        this.mensaje = 'Sus credenciales no son validas';
       }
     }
-      console.log('hello');
   }
-
   constructor(private router: Router) { }
 
   ngOnInit() {
